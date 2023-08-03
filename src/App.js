32b15e0896b1589fe13a34f30af098b8ff684c17
gabr1elpachec0 <gabrielgpacheco12@gmail.com>
2023-08-03 13:50:27 -0300

    update favorite-repository property

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
-import { React, Component } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  // Similar ao componentDidMount, pois não recebe nenhum parâmetro inicial
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://api.github.com/users/gabr1elpachec0/repos');
+      const data = await response.json();
+      setRepositories(data);
+    }
+
+    fetchData();
+  }, []);
+
+  // Similar ao componentDidUpdate, muda toda vez que o repositories atualiza
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+
+    document.title = `Você tem ${filtered.length} favorito(s)`;
+
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      // Se repo.id === id retorna repo e favorite. Senão retorna repo
+      // Se repo.favorite for true, muda para false e vice-versa. !repo.favorite possui essa lógica
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    });
+
+    setRepositories(newRepositories);
+  }
+
   return (
-    <h1>Hello World</h1>
+    <ul>
+      { repositories.map(repo => (
+        <li key={repo.id}>
+          {repo.name}
+          {repo.favorite && <span>(Favorito)</span>}
+          <button onClick={() => handleFavorite(repo.id)}> 
+            Favoritar
+          </button>
+        </li> 
+      ))}
+    </ul>
   )
 }
